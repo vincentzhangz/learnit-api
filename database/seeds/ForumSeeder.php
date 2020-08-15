2f728fe78e84b1bb8923cef8320ecac8369bd80e
HeyThisHaku <sandikalawlet@gmail.com>
2020-08-16 03:05:36 +0700

    forum and thread seeder

--- database/seeds/ForumSeeder.php
+++ database/seeds/ForumSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Course;
 use App\Forum;
+use App\Thread;
 use App\User;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
@@ -17,12 +18,23 @@ class ForumSeeder extends Seeder
     {
         $faker = Faker::create('id_ID');
         for($i =0; $i<10;$i++){
+            $forum_id = uniqid();
             $forum = new Forum;
-            $forum->forum_id = uniqid();
+            $forum->forum_id = $forum_id;
             $forum->course_id = Course::all()->random(10)->first()->course_id;
             $forum->user_id = User::all()->random(10)->first()->user_id;
             $forum->title = $faker->sentence($nbWords = 6, $variableNbWords = true);
             $forum->content = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem sint maiores veritatis consectetur voluptatum laudantium minus debitis accusantium eos dicta animi, alias sit tenetur eveniet repellat error magnam provident culpa.';
+            $forum->save();
+            for($j =0; $j<3; $j++){
+                $thread = new Thread;
+                $thread->thread_id =uniqid();
+                $thread->forum_id = $forum_id;
+                $thread->user_id = User::all()->random(10)->first()->user_id;
+                $thread->reply_content = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem sint maiores veritatis consectetur voluptatum laudantium minus debitis accusantium eos dicta animi, alias sit tenetur eveniet repellat error magnam provident culpa.';
+                $thread->iscorrect = $faker->randomElement(['true','netral','false']);
+                $thread->save();
+            }
         }
     }
 }
